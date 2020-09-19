6cacc3f80114dbed6dd5d3b5ecd49a15674be166
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-19 16:08:19 -0400

    finished the table element and have made it exportable. Need to add sortability through other functions.

---
+++ react-employee-directory/src/components/dataTable.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import dataBody from "./dataBody";
+import DataAreaContext from "../components/utils/DataAreaContext";
+
+const dataTable = () => {
+    const context = useContext(DataAreaContext);
+
+    return(
+
+        <div className="datatable mt-5">
+        <table id="table" className="table table-striped table-hover table-condensed">
+            <thead>
+                <tr>
+                    {context.developersState.headings.map(({ name, width}) => {
+                return(
+                    <th className="col" key={name} style={{ width }} onClick={() => {
+
+                        context.handleSort(name);
+                    }}
+                    >
+                        {name}
+                        <span className="pointer"></span>
+                        </th>
+                );
+                
+                    })}
+                </tr>
+            </thead>
+
+            <dataBody />
+            </table>
+            </div>
+    );
+}

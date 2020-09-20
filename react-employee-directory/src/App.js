c0ac5769c07b0022fda0b8ace7fc01519abed48c
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-19 20:53:58 -0400

    created a wrapper for child elements.

--- react-employee-directory/src/App.js
+++ react-employee-directory/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './components/Main';
-import Wrapper from './components/Wrapper';
+import Wrapper from './components/wrapper.js';
 import Header from './components/Header';
 import "./App.css";
 

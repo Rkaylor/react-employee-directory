b187c0eca62f371e1394eebdfd124f81f6a13983
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-19 20:46:26 -0400

    added a header file and done more fixes to direct to front end.

---
+++ react-employee-directory/src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <h3>Check out our Employees!</h3>
+      </div>
+    )
+  }
+
+export default Header;
